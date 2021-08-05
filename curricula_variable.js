0397d4e3a898606dd390f73c3317e212eb0190b3
QuadnucYard <2380433991@qq.com>
2021-08-05 23:15:56 +0800

    Support pseudo-conflict view

--- curricula_variable.js
+++ curricula_variable.js
@@ -1,4 +1,11 @@
 const compareInt = (a, b) => a < b ? -1 : (b < a ? 1 : 0);
+var sessionDict = new Map();
+
+const ConflictStatus = Object.freeze({
+    None: 0,
+    Psuedo: 1,
+    Proper: 2
+});
 
 class ClassSession {
     raw; //原始字符串分成的数组
@@ -66,6 +73,7 @@ class ClassSession {
 class Timetable {
 
     #choosen = null;
+    #courseChoice = new Map();
 
     constructor() {
         //周-日-节  第0周存储总体
@@ -82,43 +90,50 @@ class Timetable {
     }
 
     checkCourseConflict(course_id, class_id) {
-        let subcourse = curriculumDataMap[course_id].at(class_id);
-        let sessions = this.#resolveTeachingPlace(subcourse.teachingPlace);
-        return sessions.some(t => this.checkSessionConflict(t));
+        return this.#getSessions(course_id, class_id)
+            .some(t => this.checkSessionConflict(t));
+    }
+
+    getCourseConflicts(course_id, class_id) {
+        let set = new Set();
+        for (let session of this.#getSessions(course_id, class_id)) {
+            for (let i = session.tStart; i <= session.tEnd; i++) {
+                if (this.#choosen[session.week][session.day][i])
+                    set.add(this.#choosen[session.week][session.day][i].cid + "-" + this.#choosen[session.week][session.day][i].cno);
+            }
+        }
+        return set;
     }
 
     //选课
     chooseCourse(course_id, class_id) {
         //console.log("chooseCourse", course_id, class_id);
-        let course = curriculumDataMap[course_id];
-        let subcourse = course.at(class_id);
-        let sessions = this.#resolveTeachingPlace(subcourse.teachingPlace);
+        let sessions = this.#getSessions(course_id, class_id);
         if (sessions.some(t => this.checkSessionConflict(t))) return false;
         for (let session of sessions) {
-            session.cid = course_id;
-            session.cno = class_id;
             for (let i = session.tStart; i <= session.tEnd; i++) {
                 this.#choosen[session.week][session.day][i] = session;
-                //this.#choosen[session.week][session.day][i] = course_id + "-" + class_id;
-                //console.log(this.#choosen[session.week][session.day][i]);
             }
         }
         //console.log("select", course_id, class_id);
-        //console.log(this.#choosen);
+        this.#courseChoice.set(course_id, class_id);
         return true;
     }
 
     //退课
     dropCourse(course_id, class_id) {
         //console.log("drop", course_id, class_id);
-        let course = curriculumDataMap[course_id];
-        let subcourse = course.tcList.find(t => t.no == class_id);
-        let sessions = this.#resolveTeachingPlace(subcourse.teachingPlace);
+        let sessions = this.#getSessions(course_id, class_id);
         for (let session of sessions) {
             for (let i = session.tStart; i <= session.tEnd; i++) {
                 this.#choosen[session.week][session.day][i] = null;
             }
         }
+        this.#courseChoice.delete(course_id);
+    }
+
+    getCourseChoice(course_id) {
+        return this.#courseChoice.get(course_id);
     }
 
     toHtml(week) {
@@ -144,19 +159,19 @@ class Timetable {
                 </tr>`).join("")}`);
     }
 
-    toOverallHtml(week) {
+    toOverallHtml() {
         //把每周上午、下午、晚上的汇总起来
         let maps = Array.from({ length: 7 }, () => Array.from({ length: 3 }, () => new Map()));
         for (let i = 1; i <= 16; i++) { //枚举周
             for (let j = 0; j < 7; j++) { //枚举日
                 for (let k = 0; k < 5; k++) //枚举上午下午晚上的课
-                    if (this.#choosen[i][j][k] && !(this.#choosen[i][j][k].cid in maps[j][0]))
+                    if (this.#choosen[i][j][k] && !maps[j][0].has(this.#choosen[i][j][k].cid))
                         maps[j][0].set(this.#choosen[i][j][k].cid, this.#choosen[i][j][k]);
                 for (let k = 5; k < 10; k++)
-                    if (this.#choosen[i][j][k] && !(this.#choosen[i][j][k].cid in maps[j][1]))
+                    if (this.#choosen[i][j][k] && !maps[j][1].has(this.#choosen[i][j][k].cid))
                         maps[j][1].set(this.#choosen[i][j][k].cid, this.#choosen[i][j][k]);
                 for (let k = 10; k < 13; k++)
-                    if (this.#choosen[i][j][k] && !(this.#choosen[i][j][k].cid in maps[j][2]))
+                    if (this.#choosen[i][j][k] && !maps[j][2].has(this.#choosen[i][j][k].cid))
                         maps[j][2].set(this.#choosen[i][j][k].cid, this.#choosen[i][j][k]);
             }
         }
@@ -184,8 +199,15 @@ class Timetable {
                 </tr>`).join("")}`);
     }
 
+    #getSessions(course_id, class_id) {
+        return this.#resolveTeachingPlace(course_id, class_id, curriculumDataMap[course_id].at(class_id).teachingPlace);
+    }
+
     //解析教学时间地点的字符串
-    #resolveTeachingPlace(teachingPlace) {
+    #resolveTeachingPlace(course_id, class_id, teachingPlace) {
+        if (sessionDict.has(teachingPlace)) {
+            return sessionDict.get(teachingPlace);
+        }
         //console.log("resolve",teachingPlace);
         //有一个问题，周后面的内容可能会省略
         if (teachingPlace == "undefined") return [];
@@ -195,7 +217,7 @@ class Timetable {
             if (sp[i].length <= 2) sp[i][2] = sp[i + 1][2];
             if (sp[i].length <= 3) sp[i][3] = sp[i + 1][3];
         }
-        return sp.map(s => {
+        sessionDict.set(teachingPlace, sp.map(s => {
             let weekstr;
             let step = 1;
             if (s[0].endsWith(")")) {
@@ -213,11 +235,14 @@ class Timetable {
                     ...(s[2].substr(0, s[2].length - 1).split("-").map(t => parseInt(t) - 1)),
                     s[3]
                 );
+                ses.cid = course_id;
+                ses.cno = class_id;
                 ses.raw = s;
                 arr.push(ses);
             }
             return arr;
-        }).flat().sort((a, b) => compareInt(a.ordinal[0], b.ordinal[0]));
+        }).flat().sort((a, b) => compareInt(a.ordinal[0], b.ordinal[0])));
+        return sessionDict.get(teachingPlace);
     }
 
     trace() {
@@ -255,7 +280,7 @@ function initCourseList() {
 }
 
 function initTimetable() {
-    updateTimetable(scheduleShownMode==0 ? curWeek : 0);
+    updateTimetable(scheduleShownMode == 0 ? curWeek : 0);
 }
 
 function updateTimetable(week) {
@@ -275,10 +300,12 @@ function selectCourse1(course) {
 
 function selectCourse(course_id, class_id) {
     //console.log("selectCourse", course_id, class_id);
-    let target =$(`li#${course_id}-${class_id}`);
-    var selection = $(`li[id^='${course_id}'][class*='selected']`).attr("id");
-    if (selection != undefined) timetable.dropCourse(...selection.split("-"));
-    if (!target.hasClass("selected")) {
+    //console.log(timetable.getCourseConflicts(course_id, class_id));
+    //冲突的更新换个做法，先加上冲突，再上伪冲突
+    let target = $(`li#${course_id}-${class_id}`);
+    let selection = timetable.getCourseChoice(course_id);
+    if (selection != undefined) timetable.dropCourse(course_id, selection);
+    if (selection != class_id) {
         if (timetable.chooseCourse(course_id, class_id)) {
             target.addClass("selected");
             target.siblings().removeClass("selected");
@@ -289,13 +316,22 @@ function selectCourse(course_id, class_id) {
         target.removeClass("selected");
     }
     //更新一下冲突
+    //定义伪冲突：在同类课程中，如果去掉被选的那个导致其他可选，那么这个冲突就是伪冲突
     for (let course of curriculumData) {
+        let selection = timetable.getCourseChoice(course.cid);
         for (let cclass of course.tcList) {
-            let target = $(`li#${course.cid}-${cclass.no}`)/*.not(`[id^='${course_id}']`)*/;
-            if (timetable.checkCourseConflict(course.cid, cclass.no))
-                target.addClass("conflicting");
-            else
+            let target = $(`li#${course.cid}-${cclass.no}`);
+            let conflicts = timetable.getCourseConflicts(course.cid, cclass.no);
+            if (conflicts.size == 0 || conflicts.size == 1 && selection == cclass.no) {
                 target.removeClass("conflicting");
+                target.removeClass("pseudo-conflicting");
+            } else if (conflicts.size == 1 && conflicts.values().next().value.startsWith(course.cid)) {
+                target.removeClass("conflicting");
+                target.addClass("pseudo-conflicting");
+            } else {
+                target.addClass("conflicting");
+                target.removeClass("pseudo-conflicting");
+            }
         }
     }
     initTimetable();
